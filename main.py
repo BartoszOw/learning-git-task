54ec0228d8dd43d8842e90fb85dadf226030d4a2
BartoszOw <owbartosz.pl@gmail.com>
2024-01-21 13:18:25 +0100

    Add loop

--- main.py
+++ main.py
@@ -2,3 +2,8 @@ zakupy = {
     "piekarnia": ["chleb", "bulka","paczek"],
     "warzywniak": ["marchew","seler", "rukola"]
 }
+
+
+print("Lista zakupow")
+for i in zakupy:
+    print(f"Ide do {i}, kupuje tu nastepujace rzeczy {zakupy[i]}")
